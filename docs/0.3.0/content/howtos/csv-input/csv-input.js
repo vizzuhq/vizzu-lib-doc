cf4d94be9dbc8b1883806ff97b8b925aba8f1738
Laszlo Simon <simon@vizzuhq.com>
2021-10-11 10:19:31 +0200

    csv-input example simplified.

--- docs/0.3.0/content/howtos/csv-input/csv-input.js
+++ docs/0.3.0/content/howtos/csv-input/csv-input.js
@@ -1,18 +1,34 @@
 // Papa parse library included via script tag in index.html
 
-let rawdata = {} // object for collecting values from the CSV
-let data = { series: [] }; // empty object for Vizzu
+let data = { series: [], records: [] }; // empty object for Vizzu
 
 function receiveLine(results)
 {
-	for (let key in results.data)
+	let keys = Object.keys(results.data);
+
+	// if header row, we setup the series
+	if (data.series.length === 0) {
+		for (let key of keys) {
+			// Initialize all series as measure, then we will switch to dimension
+			// for columns containing strings
+			data.series.push({
+				name: key,
+				type: key == 'Year' ? 'dimension' : 'measure',
+			});
+		}
+	}
+	else
 	{
-		if (rawdata[key] === undefined) rawdata[key] = [];
-		let value = results.data[key];
-		// we need the years as strings because we want them to 
-		// be a categorical data series
-		if (key == 'Year') value = value.toString();  
-		rawdata[key].push(value);
+		// store the values as a new record
+		data.records.push(Object.values(results.data));
+
+		// check value types, if not number, then set series type to dimension
+		for (let i = 0; i < data.series.length; i++)
+		{
+			let key = data.series[i].name;
+			if (typeof results.data[key] !== 'number')
+				data.series[i].type = 'dimension';
+		}
 	}
 }
 
@@ -23,7 +39,7 @@ let dataReady = new Promise(function(complete, error)
 	// the format of the actual CSV file. See Papa.parse configs here:
 	// https://www.papaparse.com/docs#config
 	Papa.parse('https://lib.vizzuhq.com/datasets/population_total_long.csv',
-	{
+{
 		download: true, // we indicate that the passed parameter is an URL 
 		header: true, // our CSV file start with a header line
 		dynamicTyping: true, // papa will recognise number/string types and will 
@@ -31,22 +47,9 @@ let dataReady = new Promise(function(complete, error)
 		quoteChar: '"', // doublequotes are used in our CSV
 		skipEmptyLines: true, // we don't need the empty lines
 		step: receiveLine, // our callback for handling the incoming data by lines 
-		complete,
+		complete: () => { complete(data) },
 		error
 	})
-}).then(() => 
-{
-	// CSV processing is ready, we create the Vizzu compatible data object from
-	// the collected data
-	for (let name in rawdata)
-	{
-		// deciding the data series type based on the JS type
-		let type = typeof rawdata[name][0] === 'number' ? 'measure' : 'dimension';
-
-		data.series.push({ name, type, values: rawdata[name] });
-	};
-
-	return Promise.resolve(data);
-})
+});
 
 export default dataReady;
