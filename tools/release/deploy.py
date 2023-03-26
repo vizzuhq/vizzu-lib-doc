8d7e33a9fdaf6138562304526ddab6f1cba9fbb1
David Vegh <david.andras.vegh@gmail.com>
2023-03-26 13:40:20 +0200

    doc: fix edit button

--- tools/release/deploy.py
+++ tools/release/deploy.py
@@ -22,7 +22,7 @@ from vizzu import (  # pylint: disable=import-error, wrong-import-position, wron
 class Deploy:
     """A class for deploying site."""
 
-    # pylint: disable=too-few-public-methods
+    latest: bool = True
 
     @staticmethod
     def mike() -> None:
@@ -30,21 +30,52 @@ class Deploy:
 
         version = Vizzu.get_version()
 
+        params = [
+            "mike",
+            "deploy",
+        ]
+        if Deploy.latest:
+            params.append("-u")
+        params.append(version)
+        if Deploy.latest:
+            params.append("latest")
+        params.append("-F")
+        params.append("tools/mkdocs/mkdocs.yml")
+
         with Popen(
-            [
-                "mike",
-                "deploy",
-                "-u",
-                version,
-                "latest",
-                "-F",
-                "tools/mkdocs/mkdocs.yml",
-            ],
+            params,
         ) as process:
             process.communicate()
 
         if process.returncode:
             raise RuntimeError("failed to run mike")
+
+    @staticmethod
+    def set_config(restore: bool) -> None:
+        """
+        A method for setting config.
+        Args:
+            restore: A flag to restore the config.
+        """
+
+        with open(MKDOCS_PATH / "mkdocs.yml", "r", encoding="utf8") as fh_readme:
+            content = fh_readme.read()
+
+        if not restore:
+            if not Deploy.latest:
+                content = content.replace(
+                    "- content.action.edit",
+                    "# - content.action.edit",
+                )
+        else:
+            if not Deploy.latest:
+                content = content.replace(
+                    "# - content.action.edit",
+                    "- content.action.edit",
+                )
+
+        with open(MKDOCS_PATH / "mkdocs.yml", "w", encoding="utf8") as fh_readme:
+            fh_readme.write(content)
 
 
 def main() -> None:
@@ -54,7 +85,9 @@ def main() -> None:
     """
 
     with chdir(REPO_PATH):
+        Deploy.set_config(restore=False)
         Deploy.mike()
+        Deploy.set_config(restore=True)
 
 
 main()
