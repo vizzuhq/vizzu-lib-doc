cad9cf643e5dd27a0b214e199d80f2ffba11a79b
David Vegh <david.andras.vegh@gmail.com>
2023-04-19 09:31:04 +0200

    doc: refactor gen thumbnails

--- tools/mkdocs/thumbnails/gen_thumbnails.py
+++ tools/mkdocs/thumbnails/gen_thumbnails.py
@@ -1,11 +1,11 @@
 """A module for generating thumbnails."""
 
 
-import glob
+import os
 from pathlib import Path
 import shutil
 import sys
-from typing import List
+from typing import List, Optional
 
 from PIL import Image
 
@@ -34,8 +34,13 @@ class Thumbnails:
     """A class for generating thumbnails."""
 
     @staticmethod
-    def generate_images() -> None:
-        """A method for generating thumbnail images."""
+    def generate_images(src: Path) -> None:
+        """
+        A method for generating image thumbnails.
+
+        Args:
+            src: Source folder.
+        """
 
         print("Generating images...")
         with chdir(VIZZU_TEST_PATH):
@@ -45,64 +50,83 @@ class Thumbnails:
             params.append(Vizzu.get_test_version())
             params.append("--images")
             params.append("ALL")
-            params += glob.glob("test_cases/web_content/static/*/*")
-            params += glob.glob("test_cases/web_content/presets/*")
+            params += src.rglob("*.mjs")  # type: ignore
             Node.node(True, "test.js", *params)
 
     @staticmethod
-    def copy_images(sources: List[Path]) -> None:
+    def copy_images(src: Path, dst: Path, merge_subfolders: bool) -> None:
         """
         A method for copying static thumbnails.
 
         Args:
-            sources: List of source folders.
+            src: Source folder.
+            dst: destination folder.
+            merge_subfolders: Merge subfolders flag.
         """
 
         print("Copying images...")
-        for src in sources:
-            dst = REPO_PATH / "docs" / "examples" / src.name
-            dst.mkdir(parents=True, exist_ok=True)
-            for path in dst.rglob("*.png"):
-                path.unlink(missing_ok=True)
-            for path in src.rglob("*100.000%-1new.png"):
-                with Image.open(path) as image:
-                    resized = image.resize((320, 180))
-                    resized.save(dst / (path.parent.name + ".png"), format="png")
+        dst = REPO_PATH / "docs" / "examples" / src.name
+        dst.mkdir(parents=True, exist_ok=True)
+        for path in dst.rglob("*.png"):
+            path.unlink(missing_ok=True)
+        for path in src.rglob("*100.000%-1new.png"):
+            full_dst = dst
+            test_case = path.parent
+            if not merge_subfolders:
+                sub = os.path.relpath(test_case.parent, src)
+                full_dst = full_dst / sub
+                full_dst.mkdir(parents=True, exist_ok=True)
+                print(sub)
+            with Image.open(path) as image:
+                resized = image.resize((320, 180))
+                resized.save(full_dst / (test_case.name + ".png"), format="png")
 
     @staticmethod
-    def generate_videos() -> None:
-        """A method for generating thumbnail videos."""
+    def generate_videos(src: Path, gen_params: Optional[List[str]] = None) -> None:
+        """
+        A method for generating thumbnail videos.
+
+        Args:
+            src: Source folder.
+            gen_params: Parameters for generate.js.
+        """
 
         print("Generating videos...")
         with chdir(VIZZU_VIDEO_PATH):
             params = []
             params.append("--vizzu")
             params.append(Vizzu.get_test_version())
-            params += glob.glob("../../test_cases/web_content/animated/*")
+            params += src.rglob("*.mjs")  # type: ignore
+            if gen_params:
+                params += gen_params
             Node.node(True, "generate.js", *params)
             Node.node(False, "resize.js")
 
     @staticmethod
-    def copy_videos(sources: List[Path]) -> None:
+    def copy_videos(src: Path, dst: Path, merge_subfolders: bool) -> None:
         """
-        A method for copying animated thumbnails.
+        A method for copying video thumbnails.
 
         Args:
-            sources: List of source folders.
+            src: Source folder.
+            dst: destination folder.
+            merge_subfolders: Merge subfolders flag.
         """
 
         print("Copying videos...")
-        for src in sources:
-            dst = REPO_PATH / "docs" / "examples" / "animated"
-            dst.mkdir(parents=True, exist_ok=True)
-            for path in list(dst.rglob("*.webm")) + list(dst.rglob("*.mp4")):
-                path.unlink(missing_ok=True)
-            for path in list(src.rglob("*.webm")) + list(src.rglob("*.mp4")):
-                name_parts = path.name.split("__")
-                shutil.copyfile(
-                    path,
-                    dst / name_parts[-1],
-                )
+        dst.mkdir(parents=True, exist_ok=True)
+        for path in list(dst.rglob("*.webm")) + list(dst.rglob("*.mp4")):
+            path.unlink(missing_ok=True)
+        for path in list(src.rglob("*.webm")) + list(src.rglob("*.mp4")):
+            full_dst = dst
+            if not merge_subfolders:
+                sub = os.path.relpath(path.parent, src)
+                full_dst = full_dst / sub
+                full_dst.mkdir(parents=True, exist_ok=True)
+            shutil.copyfile(
+                path,
+                full_dst / path.name,
+            )
 
 
 def main() -> None:
@@ -112,32 +136,50 @@ def main() -> None:
     """
 
     with chdir(REPO_PATH):
-        Thumbnails.generate_images()
+        Thumbnails.generate_images(VIZZU_TEST_PATH / "test_cases/web_content/static/")
         Thumbnails.copy_images(
-            [
-                VIZZU_TEST_PATH
-                / "test_report"
-                / "results"
-                / "test_cases"
-                / "web_content"
-                / "presets",
-                VIZZU_TEST_PATH
-                / "test_report"
-                / "results"
-                / "test_cases"
-                / "web_content"
-                / "static",
-            ]
+            VIZZU_TEST_PATH
+            / "test_report"
+            / "results"
+            / "test_cases"
+            / "web_content"
+            / "static",
+            REPO_PATH / "docs" / "examples" / "static",
+            True,
         )
-        Thumbnails.generate_videos()
+
+        Thumbnails.generate_images(VIZZU_TEST_PATH / "test_cases/web_content/presets/")
+        Thumbnails.copy_images(
+            VIZZU_TEST_PATH
+            / "test_report"
+            / "results"
+            / "test_cases"
+            / "web_content"
+            / "presets",
+            REPO_PATH / "docs" / "examples" / "presets",
+            True,
+        )
+
+        Thumbnails.generate_videos(
+            VIZZU_TEST_PATH / "test_cases/web_content/animated/", ["--reverse"]
+        )
         Thumbnails.copy_videos(
-            [
-                VIZZU_VIDEO_PATH
-                / "resized"
-                / "test_cases"
-                / "web_content"
-                / "animated",
-            ]
+            VIZZU_VIDEO_PATH / "resized" / "test_cases" / "web_content" / "animated",
+            REPO_PATH / "docs" / "examples" / "animated",
+            True,
+        )
+
+        Thumbnails.generate_videos(
+            VIZZU_TEST_PATH / "test_cases/web_content/analytical_operations/"
+        )
+        Thumbnails.copy_videos(
+            VIZZU_VIDEO_PATH
+            / "resized"
+            / "test_cases"
+            / "web_content"
+            / "analytical_operations",
+            REPO_PATH / "docs" / "examples" / "analytical_operations",
+            False,
         )
 
 
