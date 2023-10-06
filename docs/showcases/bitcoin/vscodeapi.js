dc9d47795b7cdb4f91d789513f9ed827bbf72d29
David Vegh <veghdev@gmail.com>
2023-10-06 16:45:08 +0200

    Fix bitcoin/vscodeapi

--- docs/showcases/bitcoin/vscodeapi.js
+++ docs/showcases/bitcoin/vscodeapi.js
@@ -1,9 +1,9 @@
-var getJSON = function (url, callback) {
-  var xhr = new XMLHttpRequest()
+const getJSON = function (url, callback) {
+  const xhr = new XMLHttpRequest()
   xhr.open('GET', url, true)
   xhr.responseType = 'json'
   xhr.onload = function () {
-    var status = xhr.status
+    const status = xhr.status
     if (status === 200) {
       callback(null, xhr.response)
     } else {
@@ -17,6 +17,7 @@ function getJSONSync(url) {
   return new Promise(function (resolve, reject) {
     getJSON(url, function (err, data) {
       if (err != null)
+        // eslint-disable-next-line prefer-promise-reject-errors
         reject({
           status: 'unable to load sample data'
         })
@@ -25,17 +26,18 @@ function getJSONSync(url) {
   })
 }
 
+// eslint-disable-next-line no-unused-vars
 function acquireVsCodeApi(project) {
   return {
     postMessage: function (msgParam) {
-      if (msgParam.command == 'vizzu-ready') {
+      if (msgParam.command === 'vizzu-ready') {
         let data = null
         let datasum = null
         if (!project) project = 'vizzu-lib'
-        let jsonfile = project + '-data.json'
+        const jsonfile = project + '-data.json'
         getJSONSync(jsonfile).then((d1) => {
           data = d1
-          let jsonfile = project + '-datasum.json'
+          const jsonfile = project + '-datasum.json'
           getJSONSync(jsonfile).then((d2) => {
             datasum = d2
             window.postMessage({
