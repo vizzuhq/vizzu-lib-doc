a287a3be91321c4019a0bbda84c1b50c08298e39
David Vegh <veghdev@gmail.com>
2025-02-24 19:39:06 +0000

    Deployed 029f939 to 0.16 with MkDocs 1.6.0 and mike 2.1.2

--- 0.16/assets/javascripts/mdchart.js
+++ 0.16/assets/javascripts/mdchart.js
@@ -7,13 +7,15 @@ class MdChart {
 		this.id = id
 	}
 
+	async createFromConfig(snippets) {
+		this.create(await loadAnimations(snippets))
+	}
+
 	async create(snippets) {
-		const isTutorial = window.location.href.includes('tutorial')
-    	const animations = isTutorial ? await loadAnimations(snippets) : snippets
 		let chart = Promise.resolve()
-		for (let i = 0; i < animations.length; i++) {
+		for (let i = 0; i < snippets.length; i++) {
 			const number = i + 1
-			chart = this.animate(('0' + number).slice(-2), animations[i], chart)
+			chart = this.animate(('0' + number).slice(-2), snippets[i], chart)
 		}
 	}
 
