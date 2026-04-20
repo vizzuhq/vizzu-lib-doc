dcf9aa3b41684c2999ffa624c6c06825369e6f21
David Vegh <veghdev@gmail.com>
2026-04-20 13:06:36 +0000

    Deployed d81a7e9 to 0.18 with MkDocs 1.6.1 and mike 2.1.4

---
+++ 0.18/examples/static/stream_stacked_graph/main.js
@@ -0,0 +1,35 @@
+
+const dataLoaded = import("../../../assets/data/chart_types_eu.js");
+const mdChartLoaded = import("../../../assets/javascripts/mdchart.js");
+  
+Promise.all([dataLoaded, mdChartLoaded]).then((results) => {
+  const data = results[0].data;
+  const MdChart = results[1].default;
+  const mdchart = new MdChart(data, "example");
+
+  mdchart.create([
+    {
+      anims: [
+        (chart) =>
+		chart.animate({
+			data: Object.assign(data, {
+				filter: (record) =>
+					['AT', 'BE', 'DE', 'DK', 'ES', 'FI', 'FR', 'IT', 'NL', 'SE'].includes(
+						record.Country_code
+					)
+			}),
+			config: {
+				channels: {
+					x: ['Year', 'Joy factors'],
+					y: { set: ['Value 3 (+)', 'Country_code'], align: 'center' },
+					color: 'Country_code'
+				},
+				title: 'Stacked Stream Graph',
+				geometry: 'area'
+			}
+		})
+      ]
+    }
+  ]);
+});
+
---
+++ 0.18/tutorial/shorthands_store/08.js
@@ -0,0 +1,19 @@
+chart.animate(
+	{
+		style: null,
+		config: {
+			title: 'Store function',
+
+			channels: {
+				y: {
+					set: ['Popularity', 'Kinds'],
+					align: 'stretch'
+				},
+				x: ['Genres'],
+				label: ['Popularity']
+			},
+			color: ['Kinds']
+		}
+	},
+	0
+)
